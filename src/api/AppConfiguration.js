d5d4ffff01f64b78bc32ecedf5185cabdd257328
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-08-26 22:30:40 -0400

    Prod Prep 8/26/22

--- src/api/AppConfiguration.js
+++ src/api/AppConfiguration.js
@@ -1,5 +1,5 @@
 export default {
-    PRODUCTION_MODE: false,
+    PRODUCTION_MODE: true,
     // DEV_BASE: 'http://hourly-cloud-dev.herokuapp.com',
     DEV_BASE: 'http://127.0.0.1:8080/api/v0',
     PROD_BASE: 'https://hourly-timesheet.herokuapp.com/',
